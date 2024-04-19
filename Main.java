0702fd3dcc331a0020cd9488a192f700c8dfcebc
Kaio Paulo Ferreira Martins <kaiofm4@hotmail.com>
2024-04-18 21:08:26 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Cachorro c = new Cachorro("Bilu", TipoAnimal.MAMIFERO);
-        System.out.println(c.toString());
-        System.out.println(c.getT());
-        Gato g = new Gato()
+
     }
 }
